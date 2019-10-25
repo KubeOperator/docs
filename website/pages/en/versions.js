8559ee2e2a5af21773958e8098fcc0214885ec20
zhimin <zhimin@fit2cloud.com>
2019-10-25 19:32:47 +0800

    N/A

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary');
@@ -23,57 +16,21 @@ function Versions(props) {
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer versionsContainer">
         <div className="post">
-          <header className="postHeader">
-            <h1>{siteConfig.title} Versions</h1>
-          </header>
-          <p>New versions of this project are released every so often.</p>
-          <h3 id="latest">Current version (Stable)</h3>
+          <h3 id="latest">最新版本</h3>
           <table className="versions">
             <tbody>
               <tr>
                 <th>{latestVersion}</th>
                 <td>
-                  {/* You are supposed to change this href where appropriate
-                        Example: href="<baseUrl>/docs(/:language)/:id" */}
                   <a
                     href="https://docs.kubeoperator.io">
-                    Documentation
+                    文档
                   </a>
-                </td>
-                <td>
-                  <a href="">Release Notes</a>
                 </td>
               </tr>
             </tbody>
-          </table>
-          <p>
-            This is the version that is configured automatically when you first
-            install this project.
-          </p>
-          {/*<h3 id="rc">Pre-release versions</h3>
-          <table className="versions">
-            <tbody>
-              <tr>
-                <th>master</th>
-                <td>
-                  You are supposed to change this href where appropriate
-                        Example: href="<baseUrl>/docs(/:language)/next/:id" 
-                  <a
-                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
-                      props.language ? props.language + '/' : ''
-                    }next/doc1`}>
-                    Documentation
-                  </a>
-                </td>
-                <td>
-                  <a href={repoUrl}>Source Code</a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <p>Other text describing this section.</p>*/}
-          <h3 id="archive">Past Versions</h3>
-          <p>Here you can find previous versions of the documentation.</p>
+
+          <h3 id="archive">历史版本</h3>
           <table className="versions">
             <tbody>
               {versions.map(
@@ -82,29 +39,18 @@ function Versions(props) {
                     <tr>
                       <th>{version}</th>
                       <td>
-                        {/* You are supposed to change this href where appropriate
-                        Example: href="<baseUrl>/docs(/:language)/:version/:id" */}
                         <a
                           href={`https://docs.kubeoperator.io/${
                             props.language ? props.language + '/' : ''
                           }${version}/KubeOperator-v2.0/introduction`}>
-                          Documentation
-                        </a>
-                      </td>
-                      <td>
-                        <a href={`${repoUrl}/releases/tag/v${version}`}>
-                          Release Notes
+                          文档
                         </a>
                       </td>
                     </tr>
-                  ),
+                  )
               )}
             </tbody>
           </table>
-          <p>
-            You can find past versions of this project on{' '}
-            <a href={repoUrl}>GitHub</a>.
-          </p>
         </div>
       </Container>
     </div>
